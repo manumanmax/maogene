e26b33524bdd2506361996503727fdd776f98cc8
MANU <MANU@192.168.1.76>
2018-02-18 08:29:58 +0100

    Prepare face alignment

--- camera/src/camera/ModuleCVCameraReader.java
+++ camera/src/camera/ModuleCVCameraReader.java
@@ -6,6 +6,7 @@ import org.opencv.videoio.VideoCapture;
 import enums.ExecutionStatus;
 import module.Module;
 import pipeline.PipelineExecutionException;
+import utils.MaoLogger;
 
 public class ModuleCVCameraReader extends Module<Mat> {
 
@@ -20,13 +21,18 @@ public class ModuleCVCameraReader extends Module<Mat> {
 	@Override
 	public Mat process() throws PipelineExecutionException {
 		assert (_webcam.isOpened());
-		if (!_webcam.read(_frame)) {
-			throw new PipelineExecutionException(ExecutionStatus.restart, "Webcam image is still empty.");
-		}
-		if (_predecessor != null) {
-			return (Mat) _predecessor.process();
-		} else {
-			return _frame;
+		try {
+			if (!_webcam.read(_frame)) {
+				throw new PipelineExecutionException(ExecutionStatus.restart, "Webcam image is still empty.");
+			}
+			if (_predecessor != null) {
+				return (Mat) _predecessor.process();
+			} else {
+				return _frame;
+			}
+		} catch (Exception e) {
+			MaoLogger._logger.debug("Could'nt read the image from camera");
+			return null;
 		}
 	}
 
--- camera/src/main/Start.java
+++ camera/src/main/Start.java
@@ -8,9 +8,6 @@ import org.opencv.core.Core;
 import org.opencv.core.Mat;
 
 import camera.ModuleCVCameraReader;
-import face.ModuleFaceExtractor;
-import image.ModuleCropImage;
-import image.ModuleHistogramEqualisation;
 import pipeline.Pipeline;
 import pipeline.PipelineExecutionException;
 import ui.DatasetJFrame;
@@ -22,13 +19,13 @@ public class Start {
 	public static void main(String[] args) throws PipelineExecutionException, InterruptedException, ExecutionException {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		ModuleCVCameraReader mCVcr = new ModuleCVCameraReader(null);
-		ModuleHistogramEqualisation mhe = new ModuleHistogramEqualisation(mCVcr);
-		ModuleFaceExtractor mfe = new ModuleFaceExtractor(mhe);
-		ModuleCropImage mci = new ModuleCropImage(mfe, mhe);
+		// ModuleHistogramEqualisation mhe = new ModuleHistogramEqualisation(mCVcr);
+		// ModuleFaceExtractor mfe = new ModuleFaceExtractor(mCVcr);
+		// ModuleCropImage mci = new ModuleCropImage(mfe, mCVcr);
 
 		DatasetJFrame frame = new DatasetJFrame(CVUtils.matToBufferedImage(mCVcr.process()));
 		addAdvancedCloseListener(frame, mCVcr);
-		processLoop(mci, frame);
+		processLoop(mCVcr, frame);
 		mCVcr.closeCamera();
 	}
 
@@ -51,7 +48,7 @@ public class Start {
 		frame.addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
-				System.out.println("\nClosing it.");
+				System.out.println("Closing camera.");
 				try {
 					mCVcr.closeCamera();
 				} catch (Exception e1) {
